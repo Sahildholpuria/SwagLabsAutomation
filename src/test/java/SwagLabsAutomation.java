808bc57797eb3aa4e839c56d0b305e1824b08164
SahilDholpuria <dholpuria1999@gmail.com>
2023-09-29 11:40:24 +0530

    some test cases scenario added

--- src/test/java/SwagLabsAutomation.java
+++ src/test/java/SwagLabsAutomation.java
@@ -22,7 +22,9 @@ public class SwagLabsAutomation {
 
 //        Valid Username and Password
         String validUsername = String.valueOf(sheet.getRow(0).getCell(0));
+        System.out.println(sheet.getRow(0).getCell(0));
         String validPassword = String.valueOf(sheet.getRow(0).getCell(1));
+        System.out.println(sheet.getRow(0).getCell(1));
 
 //        Locked Username and Password
         String invalidUsername = String.valueOf(sheet.getRow(1).getCell(0));
@@ -30,13 +32,37 @@ public class SwagLabsAutomation {
         String invalidPassword = String.valueOf(sheet.getRow(1).getCell(1));
         System.out.println(sheet.getRow(1).getCell(1));
 
+//        Problem User
+        String ProblemUsername = String.valueOf(sheet.getRow(2).getCell(0));
+        System.out.println(sheet.getRow(2).getCell(0));
+        String ProblemPassword = String.valueOf(sheet.getRow(2).getCell(1));
+        System.out.println(sheet.getRow(2).getCell(1));
+
 
         chromeDriver.get("https://www.saucedemo.com/");
 
-        Thread.sleep(1000);
+//        Thread.sleep(1000);
+//        chromeDriver.findElement(By.id("user-name")).sendKeys(ProblemUsername);
+//        chromeDriver.findElement(By.id("password")).sendKeys(ProblemPassword);
+//
+//        chromeDriver.findElement(By.id("login-button")).click();
+
+//        Thread.sleep(1000);
+//
+//        chromeDriver.findElement(By.id("user-name")).sendKeys(invalidUsername);
+//        chromeDriver.findElement(By.id("password")).sendKeys(invalidPassword);
+//
+//        chromeDriver.findElement(By.id("login-button")).click();
+//
+//        Thread.sleep(1000);
+//
+//        chromeDriver.findElement(By.id("user-name")).clear();
+//        chromeDriver.findElement(By.id("password")).clear();
+
+        Thread.sleep(2000);
 
         chromeDriver.findElement(By.id("user-name")).sendKeys(validUsername);
-        chromeDriver.findElement(By.id("password")).sendKeys(validPassword);
+        chromeDriver.findElement(By.id("password")).sendKeys("secret_sauce");
 
         chromeDriver.findElement(By.id("login-button")).click();
 
